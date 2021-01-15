e26dd37bdcb16fbde120a74bff7263378e8c7d4e
steve braco <steve.braco@gmail.com>
2021-01-15 18:38:23 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(link => link.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(headerId){
+    const nav = document.getElementById(headerId)
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader('header'))
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+
+}
+window.addEventListener('scroll', scrollTop)
+
+/*==================== BUTTONS FILTER ====================*/ 
+const buttons = document.querySelectorAll('.projet__buttons')
+const filterImage = document.querySelectorAll('.projet__content')
+    
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        const filter = e.target.dataset.filter
+        console.log(filter);
+
+     const active = document.querySelector('.active')
+
+
+     if(button.classList.contains('active') == false) {
+         active.classList.remove('active')
+         button.classList.add('active')
+     }
+        
+        filterImage.forEach((item) => {
+            if (filter === 'all') {
+             
+                item.style.display = 'block'
+            } else {
+                if ( item.classList.contains(filter)) {
+                    item.style.display = 'block'
+                } else {
+                    item.style.display = 'none'
+                }
+            }
+        })
+
+    })
+} )
+    
